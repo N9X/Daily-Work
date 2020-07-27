de1ee7fb1bd70d724a2e8bec19cf915d4c868253
Nirmit Patel <62515466+N9X@users.noreply.github.com>
2020-07-27 18:22:45 +0530

    initial commit

---
+++ ds.js
@@ -0,0 +1,54 @@
+function mySet(){
+    var collection=[];
+    this.has=function(element){
+        return(collection.indexOf(element)!==-1);
+    };
+    this.values=function(){
+        return collection;
+    };
+
+};
+this.add = function(element){
+    if(!this.has(element)){
+        collection.push(element);
+        return true;
+    }
+    return false;
+};
+this. remove = function(element){
+    if(this.has(element)){
+        index=collection.indexOf(element);
+        collection.splice(index,1);
+        return true;
+    }
+    return false;
+}
+this.size=function(){
+    return collection.length;
+};
+
+this.union=function(otherSet){
+    var unionSet = new mySet();
+    var firstSet= this.values();
+    var secondSet= otherSet.values();
+    firstSet.forEach(function(e) {
+        unionSet.add(e);
+        
+    });
+    secondSet.forEach(function(e) {
+        unionSet.add(e);
+        
+    });
+    return unionSet;
+
+}
+this.intersectionSet = function(otherSet){
+    var intersectionSet =new mySet();
+    var firstSet=this.values();
+    firstSet.forEach(function(e) {
+        if(otherSet.has(e)){
+            intersectionSet.add(e)
+        }
+        
+    });
+}
